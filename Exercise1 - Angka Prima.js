f07d6e17980e411d4f2bf058198045f7192e47f8
Shandy Setiawan <shandysetiawan@Shandys-MacBook-Pro.local>
2020-04-11 15:43:02 +0700

    last commit

--- Exercise1 - Angka Prima.js
+++ Exercise1 - Angka Prima.js
@@ -4,7 +4,7 @@ function angkaPrima(angka) {
   }
 
   for (var i = 2; i < angka; i++) {
-    if (angka % i === 0) {
+    if (angka % i == 0) {
       return false;
     }
   }
@@ -14,6 +14,6 @@ function angkaPrima(angka) {
 // TEST CASES
 console.log(angkaPrima(3)); // true
 console.log(angkaPrima(7)); // true
-console.log(angkaPrima(6)); // false
+console.log(angkaPrima(12)); // false
 console.log(angkaPrima(23)); // true
 console.log(angkaPrima(33)); // false
